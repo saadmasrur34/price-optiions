f913d68ba3c70bc65677cac694270514a987c61a
saadmasrur34 <saadmasrur34@gmail.com>
2024-10-12 23:41:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+
+import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+
+function App() {
+  return (
+    <>
+      <DaisyNav></DaisyNav>
+      <h1 className='text-7xl'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
